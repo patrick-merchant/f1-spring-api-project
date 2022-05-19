7d9f89874772ad483680348c96fcc0431da7eb4f
Patrick Merchant <patrickmerchant9@gmail.com>
2022-05-19 12:37:11 +0100

    first derived query added and tested

--- f1_results/src/main/java/com/bnta/f1_results/repositories/DriverRepository.java
+++ f1_results/src/main/java/com/bnta/f1_results/repositories/DriverRepository.java
@@ -3,5 +3,10 @@ package com.bnta.f1_results.repositories;
 import com.bnta.f1_results.models.Driver;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DriverRepository extends JpaRepository<Driver, Long> {
+
+// first derived query - find driver by country
+    List<Driver> findByNationalityEquals(String nationality);
 }
--- f1_results/src/test/java/com/bnta/f1_results/F1ResultsApplicationTests.java
+++ f1_results/src/test/java/com/bnta/f1_results/F1ResultsApplicationTests.java
@@ -1,13 +1,25 @@
 package com.bnta.f1_results;
 
+import com.bnta.f1_results.repositories.DriverRepository;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class F1ResultsApplicationTests {
 
+	@Autowired
+	private DriverRepository driverRepository;
+
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void testFindByNationality() {
+		assertThat(driverRepository.findByNationalityEquals("German").size()).isEqualTo(3);
 	}
 
 }
